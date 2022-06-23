669d06e32d44acd9119ceb7c0b015b7f79a689dd
Choua Vang <cvangx286@gmail.com>
2022-06-22 23:23:27 -0500

    adding new files

--- static/js/GaugeCode.js
+++ static/js/GaugeCode.js
@@ -5,42 +5,48 @@ function buildCharts(sample) {
     console.log(data);
 
     // Create a variable that holds the samples array. 
-
+   
     // Create a variable that filters the samples for the object with the desired sample number.
-
+    
     // 1. Create a variable that filters the metadata array for the object with the desired sample number.
-
+    var metadata_data = metadata.filter(sampleObj => sampleObj.id == sample);
     // Create a variable that holds the first sample in the array.
-  
+    
 
     // 2. Create a variable that holds the first sample in the metadata array.
-    
+    metadata_data = metadata_data[0];
 
     // Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    
 
     // 3. Create a variable that holds the washing frequency.
-   
+    var wfreq = metadata_data.wfreq;
+
     // Create the yticks for the bar chart.
+    
 
     // Use Plotly to plot the bar data and layout.
-    Plotly.newPlot();
+    
     
     // Use Plotly to plot the bubble data and layout.
-    Plotly.newPlot();
+  
    
     
     // 4. Create the trace for the gauge chart.
-    var gaugeData = [
-     
-    ];
+    var gaugeData = {
+      domain: { x: [0, 1], y: [0, 1] },
+      value: wfreq,
+      title: { text: "Belly Button Washing Frequency" },
+      type: "indicator",
+      mode: "gauge+number"
+    };
     
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
-     
+       width: 600, height: 500, margin: { t: 0, b: 0 } 
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
-    Plotly.newPlot();
+    Plotly.newPlot('gauge', gaugeData, gaugeLayout);
   });
 }
